571a9115dc17362cc5e2f42b0236b2c78f1a1514
Quinton Black <blac2410@p--cs-mini-559.wlu.ca>
2015-01-19 14:53:15 -0500

    Initial commit with storyboard

---
+++ blac_a1/blac_a1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  blac_a1
+//
+//  Created by Quinton Black on 2015-01-19.
+//  Copyright (c) 2015 Quinton Black. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
