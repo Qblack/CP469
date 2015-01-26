7a3c0ff6759fd30dc996502a344a3d489d0a4795
Student <student@Students-Mac.local>
2015-01-26 10:32:13 -0500

    Added initial project with card a2

---
+++ blac_a02/blac_a02/blac_a02/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  blac_a02
+//
+//  Created by Student on 2015-01-26.
+//  Copyright (c) 2015 blac2410. All rights reserved.
+//
+
+import Foundation;
+
+struct Card {
+    let image:String;
+    let question:String;
+    let answer:String;
+}
+			
---
+++ blac_a02/blac_a02/blac_a02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  blac_a02
+//
+//  Created by Student on 2015-01-26.
+//  Copyright (c) 2015 blac2410. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
