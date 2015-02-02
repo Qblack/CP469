eeaa89472431eb48c5aaef6749d8d1a66829d7ec
Student <student@Students-Mac.local>
2015-02-02 14:11:09 -0500

    Added a03

---
+++ blac_a03/blac_a03/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  blac_a03
+//
+//  Created by Student on 2015-02-02.
+//  Copyright (c) 2015 blac2410. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
