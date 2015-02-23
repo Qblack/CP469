e79500cd2b0a392b976f6b5d72a4d9ed009d7ad9
Quinton Black <blac2410@Administrators-Mac-mini-10.local>
2015-02-23 18:51:57 -0500

    Added basic a5

---
+++ blac_a05/blac_a05/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  blac_a05
+//
+//  Created by Quinton Black on 2015-02-23.
+//  Copyright (c) 2015 Quinton Black. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
