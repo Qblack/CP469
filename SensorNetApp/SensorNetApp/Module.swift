75ca5c99f94ec0ef05ed263efd15a54ad8a2020f
Brian Sage <sage7300@p--cs-mini-511.wlu.ca>
2015-03-28 14:51:43 -0400

    Files to add

---
+++ SensorNetApp/SensorNetApp/Module.swift
@@ -0,0 +1,17 @@
+//
+//  Module.swift
+//  Final
+//
+//  Created by Brian Sage on 2015-03-28.
+//  Copyright (c) 2015 Brian Sage. All rights reserved.
+//
+
+import Foundation
+
+class Module {
+    var nodeId = -1
+    var moduleId = -1
+    var location = ""
+    var name = ""
+    var icon = ""
+}
