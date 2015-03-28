18776e41894e067d6dd8e2144cd9664585456539
Quinton Black <blac2410@p--cs-mini-511.wlu.ca>
2015-03-28 16:35:48 -0400

    Added colour picker view

---
+++ SensorNetApp/RGBViewController.swift
@@ -0,0 +1,61 @@
+//
+//  RGBViewController.swift
+//  SensorNetApp
+//
+//  Created by Quinton Black on 2015-03-28.
+//  Copyright (c) 2015 Quinton and Brian. All rights reserved.
+//
+
+import UIKit
+
+class RGBViewController: UIViewController {
+    
+    @IBOutlet weak var nodeIdLabel: UILabel!
+    @IBOutlet weak var moduleIdLabel: UILabel!
+    @IBOutlet weak var sensorsLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+
+    @IBAction func handleColourChange(sender: UIButton) {
+         println(sender.backgroundColor)
+        var colour :UIColor = sender.backgroundColor!
+        var red : CGFloat = 0.0
+        var green :CGFloat = 0.0
+        var blue : CGFloat = 0.0
+        var alpha: CGFloat = 0.0
+        
+        colour.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        println(red)
+        println(green)
+        println(blue)
+        
+        
+        
+    }
+    
+
+}
